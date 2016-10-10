c1ba3fbb3d6fc836a423a7ca382efd8c02c95eed
Matěj Simota <matejsimota@Matej-MacBook-Pro.local>
2016-10-10 17:01:43 +0200

    Initial Commit

---
+++ savethekitens/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  savethekitens
+//
+//  Created by Matěj Simota on 10.10.16.
+//  Copyright © 2016 Matěj Simota. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
